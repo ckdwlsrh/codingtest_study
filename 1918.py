cda57e6150c9778a77cd387d418350f311442c69
ckdwlsrh <ckdwls6504@gmail.com>
2021-09-13 16:22:08 +0900

    ing

--- 1918.py
+++ 1918.py
@@ -1,1 +1,33 @@
 import sys 
+
+
+stack = []
+result = ""
+
+def calc_func(s1, c, s2):
+    return s1 + s2 + c
+
+
+fm = input()
+p_cnt = 0
+
+for i in range(len(fm)):
+    if  fm[i] == '(' :
+        p_cnt+=1
+    elif fm[i] == ')' :
+        s2 = stack.pop()
+        c = stack.pop()
+        s1 = stack.pop()
+        stack.append(calc_func(s1,c,s2))
+        top = stack.pop()
+        if top == '*' or top == '/':
+            s
+        else :
+            stack.append(top)
+        p_cnt-=1
+    elif fm[i] == '*' or fm[i] == '/' :
+        
+    elif fm[i] == '+' or fm[i] == '-' :
+
+    else :
+        stack.append(fm[i])
