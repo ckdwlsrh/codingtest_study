3b5f5aac54a968bba53bfbcf192bc12931f6ac32
ckdwlsrh <ckdwls6504@gmail.com>
2021-09-14 01:14:33 +0900

    ok

--- 1918.py
+++ 1918.py
@@ -4,30 +4,32 @@ import sys
 stack = []
 result = ""
 
-def calc_func(s1, c, s2):
-    return s1 + s2 + c
-
-
 fm = input()
-p_cnt = 0
 
 for i in range(len(fm)):
-    if  fm[i] == '(' :
-        p_cnt+=1
+    if fm[i] == '(' :
+       stack.append(fm[i])
     elif fm[i] == ')' :
-        s2 = stack.pop()
-        c = stack.pop()
-        s1 = stack.pop()
-        stack.append(calc_func(s1,c,s2))
         top = stack.pop()
-        if top == '*' or top == '/':
-            s
-        else :
+        while top != '(':
+            result += top
+            top = stack.pop()
+    elif fm[i] == '+' or fm[i] == '-':
+        if stack != []:
+            top = stack.pop()
+            while top == '*' or top == '/':
+                result += top
+                top = stack.pop()
             stack.append(top)
-        p_cnt-=1
-    elif fm[i] == '*' or fm[i] == '/' :
-        
-    elif fm[i] == '+' or fm[i] == '-' :
-
-    else :
+            stack.append(fm[i])
+        else :
+            stack.append(fm[i])
+    elif fm[i] == '*' or fm[i]=='/' :
         stack.append(fm[i])
+    else :
+        result += fm[i]
+
+while stack != []:
+    result+= stack.pop()
+
+print (result)
