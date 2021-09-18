43bf3c9cf9af229b21a6ef35adeb90393ffee1cd
ckdwlsrh <ckdwls6504@gmail.com>
2021-09-18 21:59:14 +0900

    solve

--- 2346.py
+++ 2346.py
@@ -7,8 +7,12 @@ n = int(sys.stdin.readline())
 
 lst = sys.stdin.readline().split()
 
+c = lst[0]
+index = 0
+del lst[0]
+result += "1 "
 while lst is None:
-    i = lst[0]
-    len(lst)
+    lst[c]
+    
 
 print(result)
